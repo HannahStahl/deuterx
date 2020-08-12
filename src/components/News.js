0a71f730a614879201e323ea89caf339b407bb3f
Hannah Stahl <hls62@cornell.edu>
2020-08-12 14:18:27 -0400

    Added banner photo to News page

--- src/components/News.js
+++ src/components/News.js
@@ -1,15 +1,29 @@
 import React from 'react';
+import config from '../config';
 
 const News = ({ items }) => (
   <div className="news">
-    <h1>News and Events</h1>
-    <div className="news-cards">
-      {items.sort((a, b) => b.datePublished - a.datePublished).map((item) => (
-        <div className="news-card">
-          <h3>{item.itemName}</h3>
-          <p>{item.itemSubtitle}</p>
-        </div>
-      ))}
+    <img
+      src={`${config.publicCloudfrontURL}/deuterx-vials.jpg`}
+      alt={config.businessName}
+      className="fixed-image"
+    />
+    <div className="image-overlay" />
+    <div className="news-section-1">
+      <div className="news-section-1-content">
+        <h1>News and Events</h1>
+        <h3>Insert subheader for news section here</h3>
+      </div>
+    </div>
+    <div className="news-section-2">
+      <div className="news-cards">
+        {items.sort((a, b) => b.datePublished - a.datePublished).map((item) => (
+          <div className="news-card" key={item.itemId}>
+            <h3>{item.itemName}</h3>
+            <p>{item.itemSubtitle}</p>
+          </div>
+        ))}
+      </div>
     </div>
   </div>
 );
