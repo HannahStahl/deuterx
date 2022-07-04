7b613528de143397d695fafac08f7ba7abd3cbc2
Hannah Stahl <hls62@cornell.edu>
2022-07-04 12:27:24 -0500

    Show info@deuterx.com email address on staging site

--- src/config.js
+++ src/config.js
@@ -1,22 +1,7 @@
-const dev = {
-  emailURL: 'https://c0mrk8va37.execute-api.us-east-1.amazonaws.com/dev/email/send',
-  emailAddress: 'hannahstahl14@gmail.com',
-};
-
-const prod = {
-  emailURL: 'https://aiikn63n03.execute-api.us-east-1.amazonaws.com/prod/email/send',
-  emailAddress: 'info@deuterx.com',
-};
-
-// Default to dev if not set
-const config = process.env.REACT_APP_STAGE === 'prod'
-  ? prod
-  : dev;
-
 export default {
-  // Add common config values here
   publicCloudfrontURL: 'https://d17jmxltsx3ffm.cloudfront.net',
   businessName: 'DeuteRx',
   sanityURL: 'https://izvq1fr7.apicdn.sanity.io/v1/graphql/production/default',
-  ...config,
+  emailURL: 'https://aiikn63n03.execute-api.us-east-1.amazonaws.com/prod/email/send',
+  emailAddress: 'info@deuterx.com',
 };
